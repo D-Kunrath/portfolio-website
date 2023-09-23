4332ac3b6188ff574ba761ecfd1f7ed5cbe4c845
Daniel Kunrath Albanez <dkunrath@gmail.com>
2023-09-23 00:17:28 -0300

    fix: responsiveness

--- components/project.tsx
+++ components/project.tsx
@@ -31,8 +31,7 @@ export default function Project ({ title, description, tags, imageUrl }: Project
         className='
           relative bg-gray-100 max-w-[42rem] border border-black/5
           overflow-hidden rounded-lg
-          sm:pr-8 sm:h-[20rem]
-          group-even:pl-8
+          sm:pr-8 sm:h-[20rem] sm:group-even:pl-8
           hover:bg-gray-200 transition
           dark:bg-white/10 dark:hover:bg-white/20 dark:text-white
         '
@@ -40,8 +39,7 @@ export default function Project ({ title, description, tags, imageUrl }: Project
         <div
           className='
             pt-4 pb-7 px-5 flex flex-col h-full
-            sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%]
-            group-even:ml-[18rem]
+            sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] sm:group-even:ml-[18rem]
           '
         >
           <h3 className='text-2xl font-semibold'>{title}</h3>
@@ -64,6 +62,7 @@ export default function Project ({ title, description, tags, imageUrl }: Project
           alt={`Project ${title}`}
           quality={95}
           className='
+            hidden sm:block
             absolute top-8 -right-40 w-[28.25rem] shadow-2xl rounded-t-lg
             group-even:-right-[initial] group-even:-left-40
             group-hover:-translate-x-3 group-hover:-translate-y-3 group-hover:-rotate-2 group-hover:scale-[1.04]
